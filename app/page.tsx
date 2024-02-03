0fba251ea112b40991413b2e2636992608959950
rick-sys <ricksanchezy2x@gmail.com>
2024-02-04 00:44:20 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -41,14 +41,14 @@ export default async function Home() {
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Let humans do, what they really want to do
+          Building blocks for your Next project
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          Let's do things out of adventure, curiosity, joy and passion.
-          We are not the robots anymore. We built one.
+          An opinionated collection of components, hooks, and utilities for your
+          Next.js project.
         </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
