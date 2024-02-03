dfc40c5557918e333ed280105ddc7dbf75eed384
rick-sys <ricksanchezy2x@gmail.com>
2024-02-04 00:18:16 +0100

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default async function Home() {
         >
           <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
-            Create your better version
+            Introducing Precedent
           </p>
         </a>
         <h1
