082aa1693a8f55da0e204b4c049f655b4d1fcb3c
rick-sys <ricksanchezy2x@gmail.com>
2024-02-04 03:27:28 +0100

    s

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ const features = [
     title: "Beautiful, reusable components",
     description:
       "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
-    large: true,
+    large: false,
   },
   {
     title: "Performance first",
