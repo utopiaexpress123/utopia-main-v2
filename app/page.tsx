9698bd345e2d62e7e6117685664f8e413be4b929
rick-sys <ricksanchezy2x@gmail.com>
2024-02-04 03:03:45 +0100

    btn

--- app/page.tsx
+++ app/page.tsx
@@ -76,6 +76,18 @@ export default async function Home() {
             </svg>
             <p>Deploy to Vercel</p>
           </a>
+          <a
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
+            href="https://github.com/steven-tey/precedent"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+            <p>
+              <span className="hidden sm:inline-block">Star on</span> GitHub{" "}
+              <span className="font-semibold">{nFormatter(stars)}</span>
+            </p>
+          </a>
         </div>
       </div>
 
