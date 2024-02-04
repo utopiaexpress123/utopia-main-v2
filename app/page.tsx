f1b49a009f1e1f746407731d2fe71696447fa414
rick-sys <ricksanchezy2x@gmail.com>
2024-02-04 03:19:54 +0100

    fff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/home/card";
 import { DEPLOY_URL } from "@/lib/constants";
 import { Github, Twitter } from "@/components/shared/icons";
+import WebVitals from "@/components/home/web-vitals";
 import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
@@ -75,6 +76,23 @@ export default async function Home() {
             </p>
           </a>
         </div>
+      </div>
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+        {features.map(({ title, description, demo, large }) => (
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            demo={
+              title === "Beautiful, reusable components" ? (
+                <ComponentGrid />
+              ) : (
+                demo
+              )
+            }
+            large={large}
+          />
+        ))}
       </div>
     </>
   );
